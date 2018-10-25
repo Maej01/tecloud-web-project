ee526787e1c5b1e4a33efd541ad6303505cd3854
maej <maej_01@hotmail.com>
2018-10-25 15:08:43 -0400

    updated

--- app/routes/contact.js
+++ app/routes/contact.js
@@ -19,53 +19,53 @@ router.get('/contact', (req, res, next) =>{
 });
 
 router.post('/contact', urlencodedPaser, (req, res) => {
-    console.log(req.body);
-    // const output = `
-    //                 <p>You have a new contact message</p>
-    //                 <h3>Contact Details</h3>
-    //                 <ul>
-    //                     <li>name: ${req.body.name}</li>
-    //                     <li>name: ${req.body.email}</li>
-    //                     <li>name: ${req.body.phone_number}</li>
-    //                     <li>name: ${req.body.subject}</li>
-    //                 </ul>
-    //                 <h3>name: ${req.body.message}</h3>
-    //                 `;
+    // console.log(req.body);
+    const outputData = `
+                    <p>You have a new contact message</p>
+                    <h3>Contact Details</h3>
+                    <ul>
+                        <li>Name: ${req.body.name}</li>
+                        <li>Email: ${req.body.email}</li>
+                        <li>Phone: ${req.body.phone_number}</li>
+                        <li>Subject: ${req.body.subject}</li>
+                    </ul>
+                    <h3>Message: ${req.body.message}</h3>
+                    `;
 
-    // // create reusable transporter object using the default SMTP transport
-    // let transporter = nodemailer.createTransport({
-    //     service: 'gmail',
-    //     port: 25,
-    //     secure: false, // true for 465, false for other ports
-    //     auth: {
-    //         user: config.email, // generated ethereal user
-    //         pass: config.password // generated ethereal password
-    //     },
-    //     tls: {
-    //         rejectUnauthorized: false
-    //     }
-    // });
+    // create reusable transporter object using the default SMTP transport
+    let transporter = nodemailer.createTransport({
+        service: 'Gmail',
+        port: 25,
+        secure: false, // true for 465, false for other ports
+        auth: {
+            user: config.email, // generated ethereal user
+            pass: config.password// generated ethereal password
+        },
+        tls: {
+            rejectUnauthorized: false
+        }
+    });
 
-    // // setup email data with unicode symbols
-    // let mailOptions = {
-    //     from: '"Maej" <muhammad.m.sesay@tpisent.io>', // sender address
-    //     to: 'maej_01@hotmail.com', // list of receivers
-    //     subject: 'TeCloud Contact', // Subject line
-    //     text: 'Yeah it works' // plain text body
-    //     // html: output // html body
-    // };
+    // setup email data with unicode symbols
+    let mailOptions = {
+        from: '"Maej" <muhammadsesay8@gmail.com>', // sender address
+        to: 'muhammad.m.sesay@tpisent.io, jessekamara@gmail.com', // list of receivers
+        subject: 'TeCloud Contact', // Subject line
+        // text: 'Yeah it works', // plain text body
+        html: outputData // html body
+    };
 
-    // // send mail with defined transport object
-    // transporter.sendMail(mailOptions, (error, info) => {
-    //     if (error) {
-    //         return console.log(error);
-    //     }
-    //     console.log('Message sent: %s', info.messageId);
-    //     // Preview only available when sending through an Ethereal account
-    //     console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
-    // });
+    // send mail with defined transport object
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            return console.log(error);
+        }
+        console.log('Message sent: %s', info.messageId);
+        // Preview only available when sending through an Ethereal account
+        console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+    });
     
-    // res.redirect("/contact");
+    res.redirect("/contact"); //redirecting the user
 });
 
 
